6233cc85224eb4c03644349fd0ca5f867b1ab490
treeone246 <treeone246@gmail.com>
2024-01-24 16:03:39 +0700

    feat: footer_fix_logos

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/home'
+import Footer from './components/footer'
 import Navbar from './components/navbar';
 
 function App() {
   return (
     <div>
-      <Home/>
+        <Navbar />
+        <Footer />
     </div>
   );
 }
